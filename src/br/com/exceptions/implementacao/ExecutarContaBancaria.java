00612e3310077cb315e6b5f4df045d50e02807ff
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-09-03 21:33:40 -0300

    Criado o executar

--- src/br/com/exceptions/implementacao/ExecutarContaBancaria.java
+++ src/br/com/exceptions/implementacao/ExecutarContaBancaria.java
@@ -2,6 +2,8 @@ package br.com.exceptions.implementacao;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 import br.com.exceptions.beans.ContaBancaria;
 
 public class ExecutarContaBancaria {
@@ -9,16 +11,16 @@ public class ExecutarContaBancaria {
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner (System.in);
 		ContaBancaria conta = new ContaBancaria(0.0, 200.0);
+		boolean resp = true;
 		
-		double quantidade;
+		while (resp == true) {
+			//Obter o valor de saldo via tela
+			// Fazer o tratamento para valores errados e deve continuar em execução
+			
 		
-		System.out.println("Valor do deposito");
-		conta.depositar(leitor.nextDouble());
-		System.out.println(conta.getSaldo());
-		
-		System.out.println("Digite o valor de saque:");
-		conta.sacar(leitor.nextDouble());
-		
+			
+			
+		}
 		
 	}
 
